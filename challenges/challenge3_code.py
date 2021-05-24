f287cd825c2313e2d3980fae3fe26a7a66e72257
Maria Jose Molina <mjmolinacontreras@gmail.com>
2021-05-25 00:45:25 +0200

    adding new challenges information

---
+++ challenges/challenge3_code.py
@@ -0,0 +1,17 @@
+# To get started, check out the "Device Simulator Express: Getting Started" command in the command pallete, which you can access with `CMD + SHIFT + P` For Mac and `CTRL + SHIFT + P` for Windows and Linux.
+
+# Getting started with CPX and CircuitPython intro on:
+# https://learn.adafruit.com/circuitpython-made-easy-on-circuit-playground-express/circuit-playground-express-library
+
+# Find example code for CPX on:
+# https://github.com/adafruit/Adafruit_CircuitPython_CircuitPlayground/tree/master/examples
+
+
+# import CPX library
+from adafruit_circuitplayground import cp
+
+# tip: cp.buttons will contain the buttons information
+
+while True:
+    # Add your code here
+    pass
